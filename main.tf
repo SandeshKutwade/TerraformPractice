32142f54a629a2296508b5587af6a7e32b6e5f3e
Sandesh Kutwade <sandeshkutwade@Sandeshs-MacBook-Air.local>
2024-04-26 07:44:28 +0530

    changing source http to ssh

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "making_git_module" {
-    source = "https://github.com/SandeshKutwade/TerraformPractice.git"
+    source = "git@github.com:SandeshKutwade/TerraformPractice.git"
     ami = var.AMI
     instance_type = var.InstanceType
     key_name = var.PublicKey
